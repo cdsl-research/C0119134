b4916de5830a13972a4dc80fde01d67d5f8267b3
KAZU-kaa <72007272+KAZU-kaa@users.noreply.github.com>
2021-12-23 12:31:25 +0900

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-$name=`hostname -I`
-#lakdjf
-locust -f locustfile.py \
---headless -u 10000 -r 1000 \
---run-time 3m \
---csv=example \
---host http://$name/ & top | grep nginx >> log.txt
+#引数には、$command "URL"
+
+$conffile="/etc/nginx/nginx.conf"
+for i in `seq 1 100`
+do
+    #confの値を変更
+    python readconf.py $conffile $i
+    #nginxのリロード
+    sudo ngiinx -t
+    sudo nginx -s reload 
+    #実験
+    locust -f locustfile.py \
+    --headless -u 10000 -r 1000 \
+    --run-time 3m \
+    --csv=example$i \
+    --host $1 & top -b -n 180 -d 1 | grep nginx >> log$i.txt
+
+
+done
