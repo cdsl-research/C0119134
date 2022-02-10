d136b20024d043fbaab20967ea1b9e61507cc564
KAZU-kaa <72007272+KAZU-kaa@users.noreply.github.com>
2022-02-10 16:47:01 +0900

    Delete locustfile.py

--- locustfile.py
+++
@@ -1,7 +0,0 @@
-from locust import FastHttpUser, task
-import resource
-resource.setrlimit(resource.RLIMIT_NOFILE, (100000, 100000))
-class MyUser(FastHttpUser):
-    @task
-    def index(self):
-        response = self.client.get("/ubuntu/")
